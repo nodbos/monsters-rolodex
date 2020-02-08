e607b8db9aacbf56018c6d87b71c96f9ee04ab3e
nodbos <felipe.avendano.nb@gmail.com>
2020-02-08 16:58:34 -0500

    Updates

---
+++ precache-manifest.d49e326f1c91465f27527d6ce9d60102.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf39a2246906c3ea61d9d35ad2a77f4",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "b7fe4c2d5e5dfff25ccb",
+    "url": "/monsters/static/css/main.8cb1bbfb.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7fe4c2d5e5dfff25ccb",
+    "url": "/monsters/static/js/main.9229c7e2.chunk.js"
+  },
+  {
+    "revision": "27edc721a646a18481ad",
+    "url": "/monsters/static/js/runtime-main.9b332845.js"
+  }
+]);
